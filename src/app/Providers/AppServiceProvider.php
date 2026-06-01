620eb57a300cb373ce5710820a23377c0afb1cac
AminAhadian <amin.ahadiyan.work@gmial.com>
2026-06-01 10:27:18 +0330

    fix: force HTTPS scheme behind reverse proxy

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
@@ -19,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        # force requests to https for looping problem in login process
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
